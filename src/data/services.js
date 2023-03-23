d6f43c75702051e996fb187fbbfe3d4dd7539472
Nahuel Uboldi <nahueluboldi@gmail.com>
2023-03-23 18:40:56 -0300

    vite config update

--- src/data/services.js
+++ src/data/services.js
@@ -1,23 +1,28 @@
+import dimensionamientoImg from '../assets/img/dimensionamiento.svg';
+import administracionImg from '../assets/img/administracion.svg';
+import uxImg from '../assets/img/ux.svg';
+import designImg from '../assets/img/design.svg';
+
 const services = [
   {
     title: 'Dimensionamiento del problema',
     desc: 'Aplicamos técnicas para entender de forma detallada la problemática que tu organización busca solucionar. Esto permite diseñar soluciones que se vinculen de forma directa a los problemas que tu organización se oriente. Así, aseguramos que nuestros clientes cumplan con el propósito de su organización.',
-    img: '../assets/img/dimensionamiento.svg',
+    img: dimensionamientoImg,
   },
   {
     title: 'Administración de proyectos',
     desc: 'El éxito de los proyectos se asocia con realizar una planeación y evaluación de la ejecución con herramientas que atiendan cada contexto del proyecto. De esta forma personalizamos el seguimiento de los proyectos y ayudamos a nuestros clientes a lograr los resultados planeados.',
-    img: '../assets/img/administracion.svg',
+    img: administracionImg,
   },
   {
     title: 'UX Research',
     desc: 'Utilizamos las mejores técnicas de investigación de experiencia de usuario adaptadas a tu itinerario y presupuesto.',
-    img: '../assets/img/ux.svg',
+    img: uxImg,
   },
   {
     title: 'Diseño de Servicios',
     desc: 'Te brindamos asesoría para el montaje de tu emprendimiento.',
-    img: '../assets/img/design.svg',
+    img: designImg,
   },
 ];
 
